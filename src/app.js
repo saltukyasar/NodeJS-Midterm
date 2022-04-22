b87c69034437401965a8acbdf09d817b8565fef9
saltukyasar <saltukyasar@gmail.com>
2022-04-22 10:21:16 +0300

    10:20 commmiti

--- src/app.js
+++ src/app.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const path = require('path');
+const geocode = require('./utils/test_geocode')
 
-const homepage = __dirname 'public/index.html'
+const homepage = path.join(__dirname, '../public') 
+
+
 
 
 const app = express();
 
+
+app.use(express.static(homepage))
+
+
 app.get('/', (req,res)=>{
-    res.send(homepage);
+    res.render();
 })
 
 app.get('/geocode' , (req,res)=>{
-    res.send()
+    const city = req.query.city;
+    geocode(city, (err, {enlem, boylam, konum}) => {
+        res.send("Enlem : " + enlem + "Boylam " + boylam)
+        if(err){
+            return res.send(err)
+        }
 })
+
+})
+
+app.get('/forecast', (req,res)=>{
+    
+})
+
+
 
 
 const port = process.env.PORT || 3000;
